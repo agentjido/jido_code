9a46f4181c7340059e69f226f07f0ffb68a71d51
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-02-03 13:10:33 -1000

    fix(ui): handle terminal exec results

--- test/agent_jido_web/controllers/page_controller_test.exs
+++ test/agent_jido_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule AgentJidoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Agent Jido"
   end
 end
